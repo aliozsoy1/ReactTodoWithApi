b6ac578d7e3ad039c361b04c43f6d4b9512f3fd0
Ali Özsoy <aliozsoy237@gmail.com>
2022-02-20 16:01:30 +0300

    Connected with Api

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
 import logo from './logo.svg';
 import './App.css';
+import { Component } from 'react';
+import axios from 'axios';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = { todos: [] };
+
+  async componentDidMount() {
+    let result = await axios.get("https://jsonplaceholder.typicode.com/todos");
+    await new Promise(x => setTimeout(x, 1000));
+    this.setState({ todos: result.data });
+  }
+  render() {
+    return (
+      <div className="container">
+        
+      </div>
+    )
+  }
 }
 
 export default App;
